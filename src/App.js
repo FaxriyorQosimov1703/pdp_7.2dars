8f9f336d6e4d03dfdf88a47b93b1cd5fd1aaac1c
Faxriyor <faxriyor@gmail.com>
2021-06-30 16:56:49 +0300

    add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Users from "./Users";
+import Users1 from "./Users1";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    Newusers: [],
+
+    actives: true,
+
+    users: [
+      {
+        name: "sad",
+      },
+      {
+        name: "ssdfasad",
+      },
+      {
+        name: "saadfasd",
+      },
+    ],
+  };
+
+  toActiveTransfer = () => {
+    this.setState({
+      actives: false,
+    });
+  };
+
+  tousersTransfer = () => {
+    this.setState({
+      actives: true,
+    });
+  };
+  render() {
+    const { actives, users, Newusers } = this.state;
+    return (
+      <div>
+        <div className="border">
+          <div className="row">
+            <div className="col-6 border border-right m-3">
+            <div className="border m-3">
+                    <h1>Users</h1>
+                </div>
+
+            {
+                actives?<Users touserscard={this.toActiveTransfer}/>: ''
+            }
+
+
+            </div>
+
+
+
+            <div className="col-4 m-4">
+              <div className="border">
+                <h1>Activites</h1>
+              </div>
+
+            {
+                !actives?<Users1 toactivecard={this.tousersTransfer} />: ''
+            }
+
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
